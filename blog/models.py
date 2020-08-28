08a5a5904c22fb060c0ec764b07dffa66beb2e09
fegde.mayur <fegde.mayur@gmail.com>
2020-08-28 20:32:51 +0530

    Added Post Model

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,13 @@ class Blog(models.Model):
     tag = models.CharField(max_length=30)
 
 
+class Post(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.DO_NOTHING)
+    headline = models.CharField(max_length=200)
+    body_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modifies_at = models.DateTimeField(auto_now=True)
+
+
+
 
